50e0080e55431b183bba1d148f1d0cd011993879
Deepak Singh <theartistnw@gmail.com>
2022-03-05 23:49:00 +0530

    done

--- App.js
+++ App.js
@@ -1,54 +1,120 @@
-import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-export default class App extends Component {
+class ProductScanRNCamera extends Component {
 
- 
+  constructor(props) {
+    super(props);
+    this.camera = null;
+    this.barcodeCodes = [];
+
+    this.state = {
+      camera: {
+        type: RNCamera.Constants.Type.back,
+        flashMode: RNCamera.Constants.FlashMode.auto,
+      }
+    };
+  }
+
+  onBarCodeRead(scanResult) {
+    console.warn(scanResult.type);
+    alert("scan Result :- " + scanResult.data);
+    if (scanResult.data != null) {
+      if (!this.barcodeCodes.includes(scanResult.data)) {
+        this.barcodeCodes.push(scanResult.data);
+        console.warn('onBarCodeRead call');
+      }
+    }
+    return;
+  }
+
+  async takePicture() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  }
+
+  pendingView() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'lightgreen',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>Waiting</Text>
+      </View>
+    );
+  }
 
   render() {
     return (
       <View style={styles.container}>
-      <RNCamera
-        ref={ref => {
-          this.camera = ref;
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.off}
- 
-        onBarCodeRead={( barcodes ) => {
-          console.log(barcodes.data,barcodes.type);
-        }}
-        
-      />
-      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-        <TouchableOpacity onPress={()=>this.takePicture()} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SNAP </Text>
-        </TouchableOpacity>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          defaultTouchToFocus
+          flashMode={this.state.camera.flashMode}
+          mirrorImage={false}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          onFocusChanged={() => { }}
+          onZoomChanged={() => { }}
+          style={styles.preview}
+          type={this.state.camera.type}
+        />
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+          <Button
+            onPress={() => { console.log('scan clicked'); }}
+            style={styles.enterBarcodeManualButton}
+            title="Enter Barcode"
+          />
+        </View>
       </View>
-    </View>
-    )
+    );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    flex: 1
   },
   preview: {
     flex: 1,
-    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center'
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  enterBarcodeManualButton: {
     padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 40
   },
-});
+  scanScreenMessage: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
+
+export default ProductScanRNCamera;
